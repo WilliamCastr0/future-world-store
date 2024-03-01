c3d3b28e6306e704689afe2f446af4eb1736877c
William Castro <wmcjimenez@gmail.com>
2024-03-01 08:40:24 -0500

    feat: :sparkles: First commit

---
+++ src/app/store/[category]/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <main>
+            <nav>
+                Categories navigation
+            </nav>
+            {children}
+        </main>
+    )
+}
