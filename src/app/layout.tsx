6957b0249bebd22fc7accc9350e59ef612c68a7c
William Castro <wmcjimenez@gmail.com>
2024-03-06 12:23:54 -0500

    feat: :lipstick: Add global styles in the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
+
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
+import 'app/sass/globals.sass';
 
 const inter = Inter({ subsets: ["latin"] });
 
