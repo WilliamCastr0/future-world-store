1aa7f31fd0d3a6040122c834a10359073c7c8ecf
William Castro <wmcjimenez@gmail.com>
2024-03-06 12:43:57 -0500

    feat: :bento: Add assets folder & example to include them in the app
    Image added & some styles were implemented in Description component

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,13 @@
+import styles from './Description.module.sass'
+
 export const Description = () => {
     return (
-        <section>
-            <h1>Description</h1>
+        <section className={styles.Description}>
+            <img src="/images/description.jpeg" alt="products marketplace" />
+            <div className={styles.Description__text}>
+                <h2>Bring the future today!</h2>
+                <p>Future World: Your Gateway to Tomorrow&apos;s Tech!</p>
+            </div>
         </section>
     )
 }
