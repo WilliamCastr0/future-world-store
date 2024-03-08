77afe915dc488c3a05a142396f3034e45fa99b63
William Castro <wmcjimenez@gmail.com>
2024-03-08 12:15:41 -0500

    feat: :sparkles: Add Image component to manage assets in Description
    Image component added in the Description component. Also some styles were added to Header component

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
+import styles from './Header.module.sass'
+
 export const Header = () => {
     return (
         <header>
             <nav>
-                <ul>
+                <ul className={styles.Header__list}>
                     <Link href="/" target="_self">
                         <li>Home</li>
                     </Link>
