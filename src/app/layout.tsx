7d491a0718aaebc549de632ec0f1433763d05486
William Castro <wmcjimenez@gmail.com>
2024-03-15 16:54:14 -0500

    feat: :art: Add route grouping to improve home layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import { Roboto } from 'next/font/google'
 
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
-import { Hero } from "app/components/home/Hero"
-import { Description } from "app/components/home/Description"
 
 import 'app/sass/globals.sass';
 
@@ -21,8 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <Header />
-        <Hero />
-        <Description />
         {children}
         <Footer />
       </body>
