5fd45caad0603ba95ac8f4a6bb728c523a14089e
William Castro <wmcjimenez@gmail.com>
2024-03-15 16:18:27 -0500

    feat: :sparkles: Add loading.tsx component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import { Roboto } from 'next/font/google'
 
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
+import { Hero } from "app/components/home/Hero"
+import { Description } from "app/components/home/Description"
+
 import 'app/sass/globals.sass';
 
 const roboto = Roboto({
@@ -18,6 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <Header />
+        <Hero />
+        <Description />
         {children}
         <Footer />
       </body>
---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+export default function loading() {
+    return (
+        <h3>Loading...</h3>
+    )
+}
